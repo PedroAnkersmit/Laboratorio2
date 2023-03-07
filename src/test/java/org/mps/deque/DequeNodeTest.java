69e78075956e4fdb8a2eafd24a3dfe8659646f1b
Dani-gr <dani.gr@uma.es>
2023-03-07 12:22:12 +0100

    Terminados tests de DequeNodeTest

--- src/test/java/org/mps/deque/DequeNodeTest.java
+++ src/test/java/org/mps/deque/DequeNodeTest.java
@@ -8,17 +8,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * @author Pedro Ankersmit Carri�n
- * @author Daniel Garc�a Rodr�guez
+ * @author Pedro Ankersmit Carrion
+ * @author Daniel Garcia Rodriguez
  */
 class DequeNodeTest {
-    private DequeNode<Integer> node, node2, node3;
+    private DequeNode<Integer> node, node2, node3, node4;
 
     @BeforeEach
     void setUp() {
         node = new DequeNode<>(10, null, null);
         node2 = new DequeNode<>(null, null, node3);
         node3 = new DequeNode<>(-7, node2, null);
+        node4 = new DequeNode<>(1, node3, node2);
     }
 
     @Nested
@@ -41,7 +42,6 @@ class DequeNodeTest {
 
     @Nested
     @DisplayName("The method setItem()")
-
     class setItem {
         @Test
         @DisplayName("sets the item null when null is given.")
@@ -70,10 +70,10 @@ class DequeNodeTest {
         }
 
         @Test
-        @DisplayName("returns the non-null node when it is not null.")
+        @DisplayName("returns the non-null previous node when it is not null.")
         void getPreviousNotNull() {
             assertNotNull(node3.getPrevious());
-            assertEquals(10, node.getItem());
+            assertEquals(node2, node.getPrevious());
         }
     }
 
@@ -96,23 +96,119 @@ class DequeNodeTest {
         }
     }
 
-    @Test
-    void getNext() {
+    @Nested
+    @DisplayName("The method getNext()")
+    class getNext {
+        @Test
+        @DisplayName("returns null when the next node is null.")
+        void getNextNull() {
+            assertNull(node.getNext());
+        }
+
+        @Test
+        @DisplayName("returns the non-null next node when it is not null.")
+        void getNextNotNull() {
+            assertNotNull(node2.getNext());
+            assertEquals(node3, node.getNext());
+        }
     }
 
-    @Test
-    void setNext() {
+    @Nested
+    @DisplayName("The method setNext()")
+    class setNext {
+        @DisplayName("sets the next node null when null is given.")
+        @Test
+        void setNextNull() {
+            node2.setNext(null);
+            assertNull(node2.getNext());
+        }
+
+        @DisplayName("sets the next node to the given non-null node.")
+        @Test
+        void setNextNotNull() {
+            node.setNext(node3);
+            assertNotNull(node.getNext());
+            assertEquals(node3, node.getNext());
+        }
     }
 
-    @Test
-    void isFirstNode() {
+    @Nested
+    @DisplayName("The method isFirstNode()")
+    class isFirstNode {
+        @Test
+        @DisplayName("returns true if it has no previous node.")
+        void previousNull() {
+            assertNull(node.getPrevious());
+            assertTrue(node.isFirstNode());
+        }
+
+        @Test
+        @DisplayName("returns false if it has a previous node.")
+        void previousNotNull() {
+            assertNotNull(node3.getPrevious());
+            assertFalse(node3.isFirstNode());
+        }
     }
 
-    @Test
-    void isLastNode() {
+    @Nested
+    @DisplayName("The method isLastNode()")
+    class isLastNode {
+        @Test
+        @DisplayName("returns true if it has no next node.")
+        void nextNull() {
+            assertNull(node.getNext());
+            assertTrue(node.isLastNode());
+        }
+
+        @Test
+        @DisplayName("returns false if it has a next node.")
+        void nextNotNull() {
+            assertNotNull(node2.getNext());
+            assertFalse(node2.isLastNode());
+        }
     }
 
-    @Test
-    void isNotATerminalNode() {
+    @Nested
+    @DisplayName("The method isNotATerminalNode()")
+    class isNotATerminalNode {
+        @Test
+        @DisplayName("returns true if it is not first nor last.")
+        void notFirstNorLast() {
+            assertAll(
+                    () -> assertFalse(node4.isFirstNode()),
+                    () -> assertFalse(node4.isLastNode()),
+                    () -> assertTrue(node4.isNotATerminalNode())
+            );
+        }
+
+        @Test
+        @DisplayName("returns false if it is first and not last.")
+        void firstNotLast() {
+            assertAll(
+                    () -> assertTrue(node2.isFirstNode()),
+                    () -> assertFalse(node2.isLastNode()),
+                    () -> assertFalse(node2.isNotATerminalNode())
+            );
+        }
+
+        @Test
+        @DisplayName("returns false if it is last and not first.")
+        void lastNotFirst() {
+            assertAll(
+                    () -> assertFalse(node3.isFirstNode()),
+                    () -> assertTrue(node3.isLastNode()),
+                    () -> assertFalse(node3.isNotATerminalNode())
+            );
+        }
+
+        @Test
+        @DisplayName("returns false if it is both first and last.")
+        void firstAndLast() {
+            assertAll(
+                    () -> assertTrue(node.isFirstNode()),
+                    () -> assertTrue(node.isLastNode()),
+                    () -> assertFalse(node.isNotATerminalNode())
+            );
+        }
     }
 }
